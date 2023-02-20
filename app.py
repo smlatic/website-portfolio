371ed50d96d2b0e96368c614f23aa151e2887e7e
smlatic <edin.smlatic@gmail.com>
2023-02-20 01:49:42 +0100

    check

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 from flask import Flask, render_template, request, flash
+import os
+import openai
+
+
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def submit():
+    response = request.form['name']
+    return render_template("index.html", response=response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# import os
+# import openai
+
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# response = openai.Completion.create(
+#   model="text-davinci-003",
+#   prompt="",
+#   temperature=0.5,
+#   max_tokens=256,
+#   top_p=1,
+#   frequency_penalty=0,
+#   presence_penalty=0
+# )
